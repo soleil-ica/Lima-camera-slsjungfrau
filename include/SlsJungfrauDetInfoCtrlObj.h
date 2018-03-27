23afec26fb02bfa3cdab23ad8647319a6ac9422e
CedricCastel <cedric.castel@synchrotron-soleil.fr>
2018-03-27 15:41:03 +0200

    canevas creation

---
+++ include/SlsJungfrauDetInfoCtrlObj.h
@@ -0,0 +1,113 @@
+//###########################################################################
+// This file is part of LImA, a Library for Image Acquisition
+//
+// Copyright (C) : 2009-2018
+// European Synchrotron Radiation Facility
+// BP 220, Grenoble 38043
+// FRANCE
+//
+// This is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//###########################################################################
+
+/*************************************************************************/
+/*! 
+ *  \file   SlsJungfrauDetInfoCtrlObj.h
+ *  \brief  SlsJungfrau detector info class interface 
+ *  \author Cédric Castel - SOLEIL (MEDIANE SYSTEME - IT consultant) 
+*/
+/*************************************************************************/
+
+#ifndef SLSJUNGFRAUDETINFOCTRLOBJ_H
+#define SLSJUNGFRAUDETINFOCTRLOBJ_H
+
+#include "lima/Debug.h"
+#include "SlsJungfrauCompatibility.h"
+#include "lima/HwDetInfoCtrlObj.h"
+
+namespace lima 
+{
+    namespace SlsJungfrau 
+    {
+        class Camera;
+
+        /***********************************************************************
+         * \class DetInfoCtrlObj
+         * \brief Control object providing SlsJungfrau detector info interface
+         ***********************************************************************/
+
+        class LIBSLSJUNGFRAU_API DetInfoCtrlObj: public HwDetInfoCtrlObj 
+        {
+	        DEB_CLASS_NAMESPC(DebModCamera, "DetInfoCtrlObj", "SlsJungfrau");
+
+        public:
+            // constructor
+            DetInfoCtrlObj(Camera & cam);
+
+            // destructor
+	        virtual ~DetInfoCtrlObj();
+
+            //==================================================================
+            // image size management
+            //==================================================================
+            // gets the maximum image size
+	        virtual void getMaxImageSize(Size & max_image_size);
+	        
+            // gets the detector image size
+            virtual void getDetectorImageSize(Size & det_image_size);
+
+            // ?
+	        virtual void getDefImageType(ImageType & def_image_type);
+
+            //==================================================================
+            // current image type management
+            //==================================================================
+            // gets the current image type
+	        virtual void getCurrImageType(ImageType & curr_image_type);
+
+            // sets the current image type
+	        virtual void setCurrImageType(ImageType curr_image_type);
+
+            //==================================================================
+            // pixel size management
+            //==================================================================
+            // gets the pixel size
+            virtual void getPixelSize(double & x_size, double & y_size);
+
+            //==================================================================
+            // detector info management
+            //==================================================================
+            // gets the detector type
+            virtual void getDetectorType(std::string & det_type);
+
+            // gets the detector model
+            virtual void getDetectorModel(std::string & det_model);
+
+            //==================================================================
+            // maximum image size callback management
+            //==================================================================
+            // registers maximum image size callback
+	        virtual void registerMaxImageSizeCallback(HwMaxImageSizeCallback & cb);
+	        
+            // unregisters maximum image size callback
+            virtual void unregisterMaxImageSizeCallback(HwMaxImageSizeCallback & cb);
+
+        private:
+	        Camera & m_cam;
+        };
+    } // namespace SlsJungfrau
+} // namespace lima
+
+#endif // SLSJUNGFRAUDETINFOCTRLOBJ_H
+
+/*************************************************************************/
---
+++ include/SlsJungfrauSyncCtrlObj.h
@@ -0,0 +1,114 @@
+//###########################################################################
+// This file is part of LImA, a Library for Image Acquisition
+//
+// Copyright (C) : 2009-2018
+// European Synchrotron Radiation Facility
+// BP 220, Grenoble 38043
+// FRANCE
+//
+// This is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//###########################################################################
+
+/*************************************************************************/
+/*! 
+ *  \file   SlsJungfrauSyncCtrlObj.h
+ *  \brief  SlsJungfrau synchronization class interface 
+ *  \author Cédric Castel - SOLEIL (MEDIANE SYSTEME - IT consultant) 
+*/
+/*************************************************************************/
+
+#ifndef SLSJUNGFRAUSYNCCTRLOBJ_H
+#define SLSJUNGFRAUSYNCCTRLOBJ_H
+
+#include "lima/Debug.h"
+#include "SlsJungfrauCompatibility.h"
+#include "lima/HwSyncCtrlObj.h"
+#include "lima/HwInterface.h"
+
+namespace lima
+{
+    namespace SlsJungfrau
+    {
+        class Camera;
+
+        /***********************************************************************
+         * \class SyncCtrlObj
+         * \brief Control object providing SlsJungfrau synchronization interface
+         ***********************************************************************/
+
+        class LIBSLSJUNGFRAU_API SyncCtrlObj: public HwSyncCtrlObj
+        {
+            DEB_CLASS_NAMESPC(DebModCamera, "SyncCtrlObj", "SlsJungfrau");
+
+        public:
+            // constructor
+            SyncCtrlObj(Camera & cam);
+
+            // destructor
+            virtual ~SyncCtrlObj();
+
+            //==================================================================
+            // trigger mode management
+            //==================================================================
+            // Checks the trigger mode validity
+            virtual bool checkTrigMode(TrigMode trig_mode);
+
+            // Sets the trigger mode
+            virtual void setTrigMode(TrigMode trig_mode);
+
+            // Gets the trigger mode
+            virtual void getTrigMode(TrigMode & trig_mode);
+
+            //==================================================================
+            // exposure time management
+            //==================================================================
+            // Sets the exposure time
+            virtual void setExpTime(double exp_time);
+
+            // Gets the exposure time
+            virtual void getExpTime(double & exp_time);
+
+            //==================================================================
+            // latency time management
+            //==================================================================
+            // Sets the latency time
+            virtual void setLatTime(double lat_time);
+
+            // Gets the latency time
+            virtual void getLatTime(double & lat_time);
+
+            //==================================================================
+            // number of frames management
+            //==================================================================
+            // Sets the number of frames
+            virtual void setNbHwFrames(int nb_frames);
+
+            // Gets the number of frames
+            virtual void getNbHwFrames(int & nb_frames);
+
+            //==================================================================
+            // valid ranges management
+            //==================================================================
+            // Gets exposure and latency ranges
+            virtual void getValidRanges(ValidRangesType & valid_ranges);
+
+        private:
+            Camera & m_cam;
+        };
+    } // namespace SlsJungfrau
+} // namespace lima
+
+#endif // SLSJUNGFRAUSYNCCTRLOBJ_H
+
+/*************************************************************************/
