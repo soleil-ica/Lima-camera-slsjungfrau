d01eb09333fec32b4d1022b279a69af985681a8a
CedricCastel <cedric.castel@synchrotron-soleil.fr>
2018-04-04 16:27:28 +0200

    build config changed

--- build-sls.sh
+++ build-sls.sh
@@ -3,3 +3,5 @@
 mvn clean install -f pom_64.xml -o
 read -rsn1 -p"Press any key to continue";echo
 
+cp target/nar/lib/amd64-Linux-g++/shared/libLimaSlsJungfrau-amd64-Linux-gcc-shared-release-1.0.0.so /home/nanoscopium/CC/Jungfrau/DeviceServers/
+
